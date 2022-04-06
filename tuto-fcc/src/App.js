ff6e71027b3562691eaaed81061b0ac74e41c464
Jonathan2307 <gratigny.j@me.com>
2022-04-06 15:03:39 +0200

    init

--- tuto-fcc/src/App.js
+++ tuto-fcc/src/App.js
@@ -1,33 +1,12 @@
 import React from "react"
-import Card from "./components/Card.js"
-import Hero from "./components/Hero.js"
-
-import Navbar from "./components/Navbar.js"
-import Data from "./data.js"
-
-
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 export default function App() {
-    const cards = Data.map(item => {
-        
-        return (
-            <Card 
-            key={item.id}
-            // item={item} 
-            // dans Card, les props = props.item.location/...
-            // alors qu'avec le spread, on retire le .item (plus lisible)
-            {...item}
-            />
-            )
-        }
-        )
         return (
             <div>
-            <Navbar />
-            <Hero />
-            <section className="cards-list">
-            {cards}
-            </section>
+                <Header />
+                <Meme />
         </div>
     )
 }

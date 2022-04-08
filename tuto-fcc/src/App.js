208e09a9a2ed5686cacc219b7ee2f30a33df1696
Jonathan2307 <gratigny.j@me.com>
2022-04-08 16:49:21 +0200

    dark mode

--- tuto-fcc/src/App.js
+++ tuto-fcc/src/App.js
@@ -5,10 +5,16 @@ import Main from "./components/Main"
 
 
 export default function App() {
+    const [darkMode, setDarkMode] = React.useState(true)
+    
+    function toggleDarkMode() {
+        setDarkMode(prevMode => !prevMode)
+    }
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+            <Main darkMode={darkMode} />
         </div>
     )
 }
